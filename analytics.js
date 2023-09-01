dbaae4d6018553cf3d9b7ca094eb3ae1732f1490
irudnyts <yegor.rudnytskiy@gmail.com>
2023-09-01 13:25:03 +0000

    Deploying to gh-pages from @ trye-io/pyua@a871cdb0b1b818c735b25ff4a58b62c00151302b 🚀

---
+++ analytics.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5JCZ6HK8M9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5JCZ6HK8M9');
+</script>
