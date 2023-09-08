78ae9852562c869e2fec5c73c7fe82a2c01682fa
irudnyts <yegor.rudnytskiy@gmail.com>
2023-09-08 20:20:37 +0000

    Deploying to gh-pages from @ trye-io/pyua@74f9082d3848cc9c9469bec093dff19220426018 🚀

--- firework.js
+++ firework.js
@@ -13,43 +13,45 @@ function randomColor() {
 
 window.addEventListener('DOMContentLoaded', function() {
     const this_book = document.getElementById("this_book");
-    this_book.addEventListener("click", function () {
-        for (let j = 0; j < 10; j++) {
-            setTimeout(() => {
-                const particles = [];
-                const color = randomColor();
-                
-                const particle = document.createElement('span');
-                particle.classList.add('particle', 'move');
-                const { x, y } = randomLocation();
-                particle.style.setProperty('--x', x);
-                particle.style.setProperty('--y', y);
-                particle.style.background = color;
-                
-                this_book.appendChild(particle);
-                
-                particles.push(particle);
+    if (this_book !== null) {
+        this_book.addEventListener("click", function () {
+            for (let j = 0; j < 10; j++) {
                 setTimeout(() => {
-                    for (let i = 0; i < 50; i++) {
-                        const innerP = document.createElement('span');
-                        innerP.classList.add('particle', 'move' );
-                        innerP.style.transform=`translate(${x}, ${y})`;
-                        const xs = Math.random() * 200 - 100 + 'px' ;
-                        const ys = Math.random() * 200 - 100 + 'px' ;
-                        innerP.style.setProperty('--x', `calc(${x} + ${xs})`);
-                        innerP.style.setProperty('--y', `calc(${y} + ${ys})`);
-                        innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms';
-                        innerP.style.background = color;
-                        this_book.appendChild(innerP);
-                        particles.push(innerP);
-                    }
-                    setTimeout(()=> {
-                        particles.forEach(particle => {
-                            particle.remove();
-                        });
+                    const particles = [];
+                    const color = randomColor();
+                
+                    const particle = document.createElement('span');
+                    particle.classList.add('particle', 'move');
+                    const { x, y } = randomLocation();
+                    particle.style.setProperty('--x', x);
+                    particle.style.setProperty('--y', y);
+                    particle.style.background = color;
+                    
+                    this_book.appendChild(particle);
+                    
+                    particles.push(particle);
+                    setTimeout(() => {
+                        for (let i = 0; i < 50; i++) {
+                            const innerP = document.createElement('span');
+                            innerP.classList.add('particle', 'move' );
+                            innerP.style.transform=`translate(${x}, ${y})`;
+                            const xs = Math.random() * 200 - 100 + 'px' ;
+                            const ys = Math.random() * 200 - 100 + 'px' ;
+                            innerP.style.setProperty('--x', `calc(${x} + ${xs})`);
+                            innerP.style.setProperty('--y', `calc(${y} + ${ys})`);
+                            innerP.style.animationDuration = Math.random() * 300 + 200 + 'ms';
+                            innerP.style.background = color;
+                            this_book.appendChild(innerP);
+                            particles.push(innerP);
+                        }
+                        setTimeout(()=> {
+                            particles.forEach(particle => {
+                                particle.remove();
+                            });
+                        }, 700);
                     }, 700);
-                }, 700);
-            }, Math.random() * 300 + 200);
-        }
-    });
+                }, Math.random() * 300 + 200);
+            }
+        });
+    }
 });
